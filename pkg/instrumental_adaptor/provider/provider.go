4ae8f8403a3df31c11a278a201dcc76044b67724
Shad Beard <shadbeard81@gmail.com>
2018-08-08 10:32:34 -0400

    Moving Translator code to its own file. This makes it a bit cleaner and easier to read

--- pkg/instrumental_adaptor/provider/provider.go
+++ pkg/instrumental_adaptor/provider/provider.go
@@ -20,14 +20,9 @@ import (
 	"errors"
 	"fmt"
 	"log"
-	"regexp"
-	"strings"
-	"time"
 
 	instrumental "github.com/losant/k8s-instrumental-adaptor/pkg/instrumental_client"
 	"github.com/losant/k8s-instrumental-adaptor/pkg/provider"
-	"k8s.io/apimachinery/pkg/api/resource"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/metrics/pkg/apis/custom_metrics"
@@ -39,16 +34,12 @@ type externalMetric struct {
 	value external_metrics.ExternalMetricValue
 }
 
-type Translator struct {
-	instrumentalClient *instrumental.Client
-}
-
 type instrumentalProvider struct {
-	// instrumentalClient *instrumental.Client
 	externalMetrics []externalMetric
 	translator      *Translator
 }
 
+// NewInstrumentalProvider creates the provider for interacting with Instrumental.
 func NewInstrumentalProvider(token string, instrumentalClient *instrumental.Client) provider.MetricsProvider {
 	fmt.Println("Creating NewInstrumentalProvider")
 	translator := &Translator{
@@ -56,7 +47,6 @@ func NewInstrumentalProvider(token string, instrumentalClient *instrumental.Clie
 	}
 
 	return &instrumentalProvider{
-		// instrumentalClient: instrumentalClient,
 		externalMetrics: []externalMetric{},
 		translator:      translator,
 	}
@@ -114,57 +104,4 @@ func (ip *instrumentalProvider) ListAllExternalMetrics() []provider.ExternalMetr
 		externalMetricsInfo = append(externalMetricsInfo, metric.info)
 	}
 	return externalMetricsInfo
-}
-
-// GetRespForExternalMetric takes the response from the Instrumental client and returns a slice of ExternalMetricValue or an error.
-func (t *Translator) GetRespForExternalMetric(response *instrumental.InstrumentalMetric, metricName string) ([]external_metrics.ExternalMetricValue, error) {
-	metrics := []external_metrics.ExternalMetricValue{}
-
-	for i, v := range response.Response.Metrics {
-		values := response.Response.Metrics[i].Values
-		l := len(values.Data)
-		// Because Instrumental forces Duration to be at least twice the Resolution,
-		// (i.e. there with be at least two points) This should be reflected in the tests.
-		point := values.Data[l-2]
-		endTime := time.Unix(int64(values.Stop), 0)
-
-		value := point.Average
-		log.Printf("\n\tValue (Average): %f\n\n", value)
-		if value <= 0 {
-			// This shouldn't happen with correct query to Stackdriver
-			return nil, errors.New("Empty time series returned from Instrumental")
-		}
-		metricValue := external_metrics.ExternalMetricValue{
-			Timestamp:  metav1.NewTime(endTime),
-			MetricName: metricName,
-			MetricLabels: map[string]string{
-				"resource.type": v.Type,
-				"resource.name": v.Name,
-			},
-		}
-
-		metricValue.Value = *resource.NewMilliQuantity(int64(value*1000), resource.DecimalSI)
-		metrics = append(metrics, metricValue)
-	}
-
-	return metrics, nil
-}
-
-func getExternalMetricName(metricName string) string {
-	re := regexp.MustCompile(`\|[a-z]?`)
-	var out string
-	ak := re.FindAllStringSubmatch(metricName, -1)
-	for _, v := range ak {
-		n := strings.Replace(v[0], "|", "", -1)
-		metricName = strings.Replace(metricName, n, strings.ToUpper(n), -1)
-		out = strings.Replace(metricName, "|", "", -1)
-	}
-	return out
-}
-
-func getCustomMetricName(metricName string) string {
-	if strings.Contains(metricName, "|") {
-		return getExternalMetricName(metricName)
-	}
-	return metricName
 }
---
+++ pkg/instrumental_adaptor/provider/translator.go
@@ -0,0 +1,71 @@
+package provider
+
+import (
+	"errors"
+	"log"
+	"regexp"
+	"strings"
+	"time"
+
+	instrumental "github.com/losant/k8s-instrumental-adaptor/pkg/instrumental_client"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/metrics/pkg/apis/external_metrics"
+)
+
+type Translator struct {
+	instrumentalClient *instrumental.Client
+}
+
+// GetRespForExternalMetric takes the response from the Instrumental client and returns a slice of ExternalMetricValue or an error.
+func (t *Translator) GetRespForExternalMetric(response *instrumental.InstrumentalMetric, metricName string) ([]external_metrics.ExternalMetricValue, error) {
+	metrics := []external_metrics.ExternalMetricValue{}
+
+	for i, v := range response.Response.Metrics {
+		values := response.Response.Metrics[i].Values
+		l := len(values.Data)
+		// Because Instrumental forces Duration to be at least twice the Resolution,
+		// (i.e. there with be at least two points) This should be reflected in the tests.
+		point := values.Data[l-2]
+		endTime := time.Unix(int64(values.Stop), 0)
+
+		value := point.Average
+		log.Printf("\n\tValue (Average): %f\n\n", value)
+		if value <= 0 {
+			// This shouldn't happen with correct query to Stackdriver
+			return nil, errors.New("Empty time series returned from Instrumental")
+		}
+		metricValue := external_metrics.ExternalMetricValue{
+			Timestamp:  metav1.NewTime(endTime),
+			MetricName: metricName,
+			MetricLabels: map[string]string{
+				"resource.type": v.Type,
+				"resource.name": v.Name,
+			},
+		}
+
+		metricValue.Value = *resource.NewMilliQuantity(int64(value*1000), resource.DecimalSI)
+		metrics = append(metrics, metricValue)
+	}
+
+	return metrics, nil
+}
+
+func getExternalMetricName(metricName string) string {
+	re := regexp.MustCompile(`\|[a-z]?`)
+	var out string
+	ak := re.FindAllStringSubmatch(metricName, -1)
+	for _, v := range ak {
+		n := strings.Replace(v[0], "|", "", -1)
+		metricName = strings.Replace(metricName, n, strings.ToUpper(n), -1)
+		out = strings.Replace(metricName, "|", "", -1)
+	}
+	return out
+}
+
+func getCustomMetricName(metricName string) string {
+	if strings.Contains(metricName, "|") {
+		return getExternalMetricName(metricName)
+	}
+	return metricName
+}
